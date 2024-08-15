c8eb42d4e44df0dd651b85b079b8c0e67e611f6b
Emmanuel Bett <e.yegonbett@gmail.com>
2024-08-15 14:11:16 +0300

    initial commit

---
+++ src/components/TransactionList.vue
@@ -0,0 +1,24 @@
+<template>
+  <h3>History</h3>
+  <ul id="list" class="list">
+    <li
+      v-for="t in transactions"
+      :key="t.id"
+      :class="t.amount < 0 ? 'minus' : 'plus'"
+    >
+      {{ t.text }} <span> ksh {{ t.amount }}</span>
+      <button class="delete-btn">x</button>
+    </li>
+  </ul>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+
+const props = defineProps({
+  transactions: {
+    type: Array,
+    requried: true,
+  },
+});
+</script>
